8d2a7211b468324d12fb567adf1ef33f44e4f283
Nastassia Ananka <anast.ananko@gmail.com>
2023-07-16 17:48:25 +0200

    feat: add basic queries for user, profile, member and post

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,8 +1,16 @@
 import { Type } from '@fastify/type-provider-typebox';
-import { PrismaClient } from '@prisma/client'
-import { buildSchema, GraphQLObjectType,
-  GraphQLString, GraphQLList, GraphQLFloat,
-  GraphQLSchema } from 'graphql';
+import { PrismaClient } from '@prisma/client';
+import { GraphQLObjectType,
+  GraphQLString,
+  GraphQLList, 
+  GraphQLFloat, 
+  GraphQLSchema, 
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLEnumType
+} from 'graphql';
+
+import { UUIDType } from './types/uuid.js';
 
 const prisma = new PrismaClient();  
 
@@ -25,34 +33,136 @@ export const createGqlResponseSchema = {
   ),
 };
 
+export const MemberTypeId = new GraphQLEnumType({
+  name: "MemberTypeId",
+  values: {
+    BASIC: { 
+      value: "basic"
+    },
+    BUSINESS: {
+      value: "business"
+    },
+  }
+});
+
+export const MemberType = new GraphQLObjectType({
+  name: 'Member',
+  fields: () => ({
+    id: { type: MemberTypeId },
+    discount: { type: GraphQLFloat },
+    postsLimitPerMonth: { type: GraphQLInt },
+  }),
+});
+
+const ProfileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => ({
+    id: { type: UUIDType },
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    userId: { type: UUIDType },
+    memberTypeId: { type: MemberTypeId },
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: UUIDType },
     name: { type: GraphQLString },
     balance: { type: GraphQLFloat },
   }),
 });
 
-const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    users: {
-      type: new GraphQLList(UserType),
-    },
-  },
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: () => ({
+    id: { type: UUIDType },
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    authorId: { type: UUIDType },
+  }),
 });
 
 const queries = new GraphQLObjectType({
-  name: 'Query',
+  name: 'RootQueryType',
   fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: { type: UUIDType },
+      },
+      resolve: async (root, { id } : { id: string }) => {
+        return await prisma.user.findUnique({
+          where: {
+            id: id,
+          },
+        });
+      }
+    },
     users: {
       type: new GraphQLList(UserType),
       resolve: async () => {  
         return await prisma.user.findMany();
       }   
-    }
+    },
+    profile: {
+      type: ProfileType,
+      args: {
+        id: { type: UUIDType },
+      },
+      resolve: async (root, { id } : { id: string }) => {
+        return await prisma.profile.findUnique({
+          where: {
+            id: id
+          },
+        });
+      },
+    },
+    profiles: {
+      type: new GraphQLList(ProfileType),
+      resolve: async () => {
+        return await prisma.profile.findMany();
+      }
+    }, 
+    memberType: {
+      type: MemberType,
+      args: {
+        id: { type: MemberTypeId },
+      },
+      resolve: async (root, { id } : { id: string }) => {
+        return await prisma.memberType.findUnique({
+          where: {
+            id: id
+          }
+        });
+      },
+    },
+    memberTypes: {
+      type: new GraphQLList(MemberType),
+      resolve: async () => {
+        return await prisma.memberType.findMany();
+      }
+    },
+    post: {
+      type: PostType,
+      args: {
+        id: { type: UUIDType },
+      },
+      resolve: async (root, { id } : { id: string }) => {
+        return await prisma.post.findUnique({
+          where: {
+            id: id,
+          },
+        });
+      }
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async () => {  
+        return await prisma.post.findMany();
+      }   
+    },  
   }
 });
 
@@ -61,17 +171,3 @@ export const schema = new GraphQLSchema({
   // mutation: Mutation
 });
 
-// export const schema = buildSchema(`
-//   type User {
-//     id: ID
-//     name: String
-//     balance: String
-//   },
-//   type Profile {
-//     id: ID
-//     isMale: Boolean
-//     yearOfBirth: Int
-//     userId: String
-//     memberTypeId: String
-//   }
-// `);
