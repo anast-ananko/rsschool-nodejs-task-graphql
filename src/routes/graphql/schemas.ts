fd0aeaf82d22bb2e1c2db2c7ad55934d67b1980d
Nastassia Ananka <anast.ananko@gmail.com>
2023-07-20 15:55:57 +0200

    feat: add basic mutations

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -7,7 +7,9 @@ import { GraphQLObjectType,
   GraphQLSchema, 
   GraphQLBoolean,
   GraphQLInt,
-  GraphQLEnumType
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLInputObjectType
 } from 'graphql';
 
 import { UUIDType } from './types/uuid.js';
@@ -95,7 +97,22 @@ interface IUserType {
   subscribedToUser: IUserType[];
 }
 
-const UserType: GraphQLObjectType<IUserType, IUserType> = new GraphQLObjectType({
+interface IProfile {
+  id: string;
+  isMale: boolean;
+  yearOfBirth: number;
+  userId: string;
+  memberTypeId: string;
+}
+
+interface IPost {
+  id: string;
+  title: string;
+  content: string;
+  authorId: string;
+}
+
+const UserType: GraphQLObjectType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
     id: { type: UUIDType },
@@ -255,8 +272,228 @@ const queries = new GraphQLObjectType({
   }
 });
 
-export const schema = new GraphQLSchema({
-  query: queries,
-  // mutation: Mutation
+type userCreateInput = Pick<IUserType, 'name' | 'balance'>;
+type userUpdateInput = Pick<IUserType, 'name' | 'balance'>;
+type profileCreateInput = Omit<IProfile, 'id'>;
+type profileUpdateInput = Omit<IProfile, 'id' | 'userId'>;
+type postCreateInput = Omit<IPost, 'id'>;
+type postUpdateInput = Omit<IPost, 'id' | 'authorId'>;
+
+const CreateUserInput = new GraphQLInputObjectType({
+  name: 'CreateUserInput',
+  fields: () => ({
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  }),
 });
 
+const ChangeUserInput = new GraphQLInputObjectType({
+  name: 'ChangeUserInput',
+  fields: () => ({
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  }),
+});
+
+const CreateProfileInput = new GraphQLInputObjectType({
+  name: 'CreateProfileInput',
+  fields: () => ({
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    userId: { type: UUIDType },
+    memberTypeId: { type: MemberTypeId },
+  }),
+});
+
+const ChangeProfileInput = new GraphQLInputObjectType({
+  name: 'ChangeProfileInput',
+  fields: () => ({
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    memberTypeId: { type: MemberTypeId },
+  }),
+});
+
+const CreatePostInput = new GraphQLInputObjectType({
+  name: 'CreatePostInput',
+  fields: () => ({
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    authorId: { type: UUIDType },
+  }),
+});
+
+const ChangePostInput = new GraphQLInputObjectType({
+  name: 'ChangePostInput',
+  fields: () => ({
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+  })
+});
+
+const mutations = new GraphQLObjectType({
+  name: 'RootMutationType',
+  fields: {   
+    createUser: {
+      type: UserType,
+      args: {
+        dto: { type: CreateUserInput },
+      },
+      resolve: async (
+        root,
+        { dto } : { dto: userCreateInput }
+      ) => {
+        const newUser = await prisma.user.create({
+          data: dto
+        });
+
+        return newUser;
+      },
+    },
+    changeUser: {
+      type: UserType,
+      args: {
+        id: { type: UUIDType },
+        dto: { type: ChangeUserInput },
+      },
+      resolve: async (root, { id, dto }: { id: string, dto: userUpdateInput}) => {
+        const updatedUser = await prisma.user.update({
+          where: {
+            id: id,
+          },
+          data: dto
+        });
+
+        return updatedUser;
+      },
+    },
+    deleteUser: {
+      type: GraphQLBoolean,
+      args: {
+        id: { type: UUIDType },
+      },
+      resolve: async (root, { id }: { id: string }) => {
+        try {
+          await prisma.user.delete({
+            where: {
+              id: id,
+            },
+          });
+    
+          return true; 
+        } catch (error) {
+          return false; 
+        }
+      },
+    },   
+    createProfile: {
+      type: ProfileType,
+      args: {
+        dto: { type: CreateProfileInput },
+      },
+      resolve: async (
+        root,
+        { dto }: { dto: profileCreateInput }
+      ) => {
+        const newProfile = await prisma.profile.create({
+          data: dto,
+        });
+
+        return newProfile;
+      },
+    },
+    changeProfile: {
+      type: ProfileType,
+      args: {
+        id: { type: UUIDType },
+        dto: { type: ChangeProfileInput },
+      },
+      resolve: async (root, { id, dto } : { id: string, dto: profileUpdateInput }) => {
+        const updatedProfile = await prisma.profile.update({
+          where: {
+            id: id,
+          },
+          data: dto,
+        });
+
+        return updatedProfile;
+      },
+    },
+    deleteProfile: {
+      type: GraphQLBoolean, 
+      args: {
+        id: { type: UUIDType },
+      },
+      resolve: async (root, { id }: { id: string }) => {
+        try {
+          await prisma.profile.delete({
+            where: {
+              id: id,
+            },
+          });
+    
+          return true; 
+        } catch (error) {
+          return false; 
+        }
+      },
+    },
+    createPost: {
+      type: PostType,
+      args: {
+        dto: { type: CreatePostInput },
+      },
+      resolve: async (
+        root,
+        { dto }: { dto: postCreateInput }
+      ) => {
+        const newPost = await prisma.post.create({
+          data: dto
+        });
+
+        return newPost;
+      },
+    },
+    changePost: {
+      type: PostType,
+      args: {
+        id: { type: UUIDType },
+        dto: { type: ChangePostInput },
+      },
+      resolve: async (root, { id, dto } : { id: string, dto: postUpdateInput }) => {
+        const updatedPost = await prisma.post.update({
+          where: {
+            id: id,
+          },
+          data: dto
+        });
+
+        return updatedPost;
+      },
+    },
+    deletePost: {
+      type: GraphQLBoolean, 
+      args: {
+        id: { type: UUIDType },
+      },
+      resolve: async (root, { id }: { id: string }) => {
+        try {
+          await prisma.post.delete({
+            where: {
+              id: id,
+            },
+          });
+    
+          return true; 
+        } catch (error) {
+          return false; 
+        }
+      },
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: queries,
+  mutation: mutations
+});
